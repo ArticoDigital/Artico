bb3e073d5fa45c349005a4975aa7e35db6cfc47d
Juan2Ramos <juan2ramos@gmail.com>
2017-05-16 15:08:34 -0500

    mobile header

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+const btnMenu = document.querySelector('#Menu'),
+        nav = document.querySelector('nav') ;
+
+btnMenu.addEventListener('click',function () {
+    nav.classList.toggle('open')
+},false);
