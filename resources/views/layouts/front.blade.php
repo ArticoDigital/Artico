81612347f92cee4216842f7fae0443b608855871
Juan <juan2ramos@gmail.com>
2018-11-20 18:37:46 -0500

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -79,9 +79,19 @@
     <link rel="stylesheet" href="{{asset('css/main.min.css')}}"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/sweetalert2/6.6.1/sweetalert2.min.css">
     @yield('styles')
+        <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-P3TJXVP');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body id="#body" class="@yield('classBody')">
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P3TJXVP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header class="Header row middle center @yield('headerClass', '') scrollTarget">
 
     <div class="Header-bar row between middle">
